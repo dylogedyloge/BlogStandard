713b1d5f02b75d70cd3c69a9d08a895c2afbc59f
dylogedyloge <dylogedyloge@gmail.com>
2023-06-12 19:35:57 +0330

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import "../styles/globals.css";
+import { Poppins, Righteous } from "@next/font/google";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import { PostsProvider } from "../context/postsContext";
 
+config.autoAddCss = false;
+
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
+const righteous = Righteous({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-righteous",
+});
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => page);
+  return (
+    <UserProvider>
+      <PostsProvider>
+        <main
+          className={`${poppins.variable} ${righteous.variable}  font-body`}
+        >
+          {getLayout(<Component {...pageProps} />, pageProps)}
+        </main>
+      </PostsProvider>
+    </UserProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,30 @@
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
+import HeroImage from "../public/hero.jpg";
+import { Logo } from "../components/Logo";
+
 export default function Home() {
-  return <div />;
+  const { user } = useUser();
+  return (
+    <div className="w-screen h-screen overflow-hidden flex justify-center items-center relative">
+      <Image
+        src={HeroImage}
+        alt="hero"
+        className="absolute object-cover w-full h-full"
+      />
+      <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm">
+        <Logo />
+        <p className="mb-10">
+          Supercharge your essay writing with our SEO-friendly Essay Gerentor AI
+          app! Create captivating content that ranks higher in search results
+          and grabs the attention of your target audience. Say hello to
+          optimized essays that make a lasting impact.
+        </p>
+        <Link href="/post/new" className="btn">
+          Start
+        </Link>
+      </div>
+    </div>
+  );
 }
